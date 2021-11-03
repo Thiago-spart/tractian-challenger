29db48140ef16a0097e9e47b3ad02e8ad13f1179
Thiago <user000.000resu@gmail.com>
2021-11-03 11:04:43 -0300

    Add footer container

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+	position: absolute;
+	bottom: 0;
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+	padding: 1.5rem 2rem;
+	gap: 1rem;
+	width: 100%;
+	color: #d9d9d9;
+	background: #1f40af;
+`;
