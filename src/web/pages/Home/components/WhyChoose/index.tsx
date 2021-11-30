f19a2c12f0d71bbc3052c3713df9928664755217
Thiago <user000.000resu@gmail.com>
2021-11-30 11:56:59 -0300

    Add translations on WhyChoose section

--- src/web/pages/Home/components/WhyChoose/index.tsx
+++ src/web/pages/Home/components/WhyChoose/index.tsx
@@ -1,22 +1,48 @@
+import { useTranslation } from "next-i18next";
+
 import { Primary } from "web/components/Button/Primary";
 
 import { Container, Title, CardContainer } from "./styles";
 
 import { Card } from "./Card";
-import { CardContent } from "./CardContent";
 
 export const WhyChoose = () => {
+	const { t } = useTranslation("home");
+
+	const CardContent = [
+		{
+			title: t("whyChoose.cards.predictiveMaintenance.title"),
+			text: t("whyChoose.cards.predictiveMaintenance.text"),
+			imgSrc: "/whyChoose/icon-maintence-plataform.svg",
+		},
+		{
+			title: t("whyChoose.cards.onlineMonitoring.title"),
+			text: t("whyChoose.cards.onlineMonitoring.text"),
+			imgSrc: "/whyChoose/icon-maintence-online.svg",
+		},
+		{
+			title: t("whyChoose.cards.instantSetup.title"),
+			text: t("whyChoose.cards.instantSetup.text"),
+			imgSrc: "/whyChoose/icon-maintence-horimeter.svg",
+		},
+		{
+			title: t("whyChoose.cards.integratedCMMS.title"),
+			text: t("whyChoose.cards.integratedCMMS.text"),
+			imgSrc: "/whyChoose/icon-maintence-pump.svg",
+		},
+	];
+
 	return (
 		<Container>
 			<Title>
-				Por que escolher a <span>Tractian</span>?
+				{t("whyChoose.title")} <span>{t("whyChoose.companyName")}</span>?
 			</Title>
 
 			<CardContainer>
 				{CardContent.map(card => (
 					<Card
 						key={card.title}
-						imgAlt={card.imgAlt}
+						imgAlt={card.title.toLowerCase()}
 						imgSrc={card.imgSrc}
 						text={card.text}
 						title={card.title}
@@ -24,7 +50,7 @@ export const WhyChoose = () => {
 				))}
 			</CardContainer>
 
-			<Primary message="Começar agora!" />
+			<Primary message={t("whyChoose.buttonMessage")} />
 		</Container>
 	);
 };
